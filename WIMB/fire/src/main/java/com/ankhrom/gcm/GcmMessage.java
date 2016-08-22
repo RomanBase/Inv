51d9dff2af180fd2ee803f82d0be90afc5460a2f
Roman Horňák <hornak@pria.cz>
2016-08-22 12:42:18 +0200

    gcm message clear up

--- WIMB/fire/src/main/java/com/ankhrom/gcm/GcmMessage.java
+++ WIMB/fire/src/main/java/com/ankhrom/gcm/GcmMessage.java
@@ -3,45 +3,21 @@ package com.ankhrom.gcm;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.util.Log;
 
 import com.ankhrom.gcm.logic.GcmServerSideSender;
 import com.ankhrom.gcm.logic.LoggingService;
 import com.ankhrom.gcm.logic.Message;
-import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import java.io.IOException;
 
 public class GcmMessage {
 
     private final Context context;
-    private final GoogleCloudMessaging gcm;
 
     public GcmMessage(Context context) {
 
         this.context = context;
-        gcm = GoogleCloudMessaging.getInstance(context);
-    }
-
-    public void send(@NonNull final String id, @NonNull final String messageId, @Nullable final Bundle data) {
-
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... voids) {
-
-                try {
-                    Log.i(PlayService.GCM, "send message");
-                    gcm.send(id + "@gcm.googleapis.com", messageId, data);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                return null;
-            }
-        }.execute();
     }
 
     public void send(@NonNull final String apiKey, @NonNull final String id) {
